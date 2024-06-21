a75a1ffaec5284289c552684a399a5c2fb73a305
Macbook <macbook@hUnTeRpErRys-MacBook-Pro.local>
2024-06-21 21:17:10 +0000

    Revert "Revert "Implement user signup, login, and password reset""
    This reverts commit 1d5618b2f58aa667abb755c50eda5c1f8db1dd21.

--- videoapp/models.py
+++ videoapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Videos(models.Model):
+    title=models.TextField()
+    description=models.TextField()
+    video_file = models.FileField(upload_to='video')
+    def __str__(self):
+        return self.title
