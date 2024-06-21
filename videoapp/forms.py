67d37715aa688cd0d84369900110d1591c191395
Macbook <macbook@hUnTeRpErRys-MacBook-Pro.local>
2024-06-21 21:17:10 +0000

    Revert "Revert "Implement user signup, login, and password reset""
    This reverts commit 1d5618b2f58aa667abb755c50eda5c1f8db1dd21.

--- videoapp/forms.py
+++ videoapp/forms.py
@@ -1,6 +1,13 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 from django.contrib.auth.models import User
+from django.core.mail import send_mail
+from django.conf import settings
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from django.template.loader import render_to_string
+from django.contrib.sites.shortcuts import get_current_site
+from .tokens import account_activation_token
 
 class SignUpForm(UserCreationForm):
     password2=forms.CharField(required=True,widget=forms.PasswordInput(),label='Confirm Password')
@@ -9,10 +16,22 @@ class SignUpForm(UserCreationForm):
         fields = ('email','first_name','last_name','password1', 'password2')
     def save(self, commit=True):
         user = super(SignUpForm, self).save(commit=False)
-        user.username = self.cleaned_data['email']  # Set username to be the same as email
+        user.username = self.cleaned_data['email']
         user.email = self.cleaned_data['email']
         if commit:
             user.save()
+        return user
+    def send_verification_email(self, user, request):
+        current_site = get_current_site(request)
+        mail_subject = 'Activate your account.'
+        message = render_to_string('videoapp/account_activation_email.html', {
+            'user': user,
+            'domain': current_site.domain,
+            'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+            'token': account_activation_token.make_token(user),
+        })
+        to_email = self.cleaned_data.get('email')
+        send_mail(mail_subject, message, settings.DEFAULT_FROM_EMAIL, [to_email])
     
 
 class LoginForm(AuthenticationForm):
